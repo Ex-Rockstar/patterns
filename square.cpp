7351ff63153e1240828da0dd1b04cf27e5ec9307
Ruthika <ruthirockstar2218@gmail.com>
2023-08-07 13:26:17 +0530

    square pattern

---
+++ square.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int n;
+  cout<<"enter n";
+  cin>>n;
+  int i=1;
+  
+  while(i<=n){
+    int j=1;
+    while(j<=n){
+       cout<<"* ";
+      j++;
+    }
+    i++;
+    cout<<endl;
+    
+  }
+}
+
+
+/*
+enter n 4
+* * * * 
+* * * * 
+* * * * 
+* * * *
+*/
