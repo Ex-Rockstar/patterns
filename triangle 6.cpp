e1b1cd0b183af10c14013a85c6248eca1eed682b
Ruthika <ruthirockstar2218@gmail.com>
2023-08-07 14:26:45 +0530

    triangle 6

---
+++ triangle 6.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n;
+  cout << "enter n";
+  cin >> n;
+  int i=1;
+  while(i<=n){
+    int j=1;
+    while(j<=i){
+      cout<<i-j+1<<" ";
+      
+      j++;
+    }
+    cout<<endl;
+    i++;
+  }
+  return 0;
+}
+
+
+/*
+enter n4
+1 
+2 1 
+3 2 1 
+4 3 2 1
+*/
