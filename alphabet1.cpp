dc7909d233948f1de7fa6f8239c7f2de3ecf3763
Ruthika <ruthirockstar2218@gmail.com>
2023-08-07 14:38:32 +0530

    alphabet square 1

---
+++ alphabet1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a=3;
+    // cin>>a;
+    char c='A';
+    int i=1;
+    while(i<=a){
+        int j=1;
+        while(j<=a){
+            cout<<c;
+            j++;
+        }
+        cout<<endl;
+        
+        i++;
+        c++;
+    }
+}
