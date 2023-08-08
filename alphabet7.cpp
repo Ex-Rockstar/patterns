2e7989e018d12958845ffbbe25c69f96f7e6bd1b
Ruthika <ruthirockstar2218@gmail.com>
2023-08-08 09:22:36 +0530

    alphabet 7

---
+++ alphabet7.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+
+
+    int n=3;
+    int i=1;
+    char a='A';
+    while(i<=n){
+        int j=1;
+        while(j<=i){
+            cout<<a;
+            a++;
+            j++;
+        }
+        i++;
+        cout<<endl;
+
+    }
+}
+
+
+/*
+A
+BC
+DEF
+*/
