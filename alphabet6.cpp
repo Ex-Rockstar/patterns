526a85abae1919fd7c41ec80d4c84a5e97bc9e26
Ruthika <ruthirockstar2218@gmail.com>
2023-08-08 09:20:10 +0530

    alphabet 6

---
+++ alphabet6.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+
+
+    int n=3;
+    int i=1;
+    char a='A';
+    while(i<=n){
+        int j=1;
+        while(j<=i){
+            cout<<a;
+            j++;
+        }
+        i++;
+        a++;
+        cout<<endl;
+        
+    }
+}
