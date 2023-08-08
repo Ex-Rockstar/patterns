b00ce869308167f1c1a79703107806fab44a9375
Ruthika <ruthirockstar2218@gmail.com>
2023-08-08 09:48:57 +0530

    reverse triangle

---
+++ trianglestarrev.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n=4;
+    int i=1;
+    while(i<=n){
+        //print space
+        int space = n-i;
+        while(space){
+            cout<<" ";
+            space--;
+        }
+
+        int j=1;
+        while(j<=i){
+            cout<<"*";
+            j++;
+        }
+        cout<<endl;
+        i++;
+
+        //print star 
+
+    }
+}
