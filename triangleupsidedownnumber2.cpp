b0a8a38961b2c45817457bdad12dfee8f2235dae
Ruthika <ruthirockstar2218@gmail.com>
2023-08-09 08:51:13 +0530

    triangle upside down patterns

---
+++ triangleupsidedownnumber2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+
+
+    int n=3;
+    int i=1;
+    while(i<=n){
+    int a=i;
+        int j=1;
+        while(j<=i){
+            cout<<a;
+            j++;
+        }
+        i++;
+        a++;
+        cout<<endl;
+
+    }
+}
